7049a60ac6eb1f50dd8f732e31d13a57826e151e
userkmitl <59050300@kmitl.ac.th>
2018-08-17 06:41:49 +0700

    :sweat_smile: add pubDate

--- controllers/thlottory.js
+++ controllers/thlottory.js
@@ -7,7 +7,7 @@ const  ThLotory = async function (req, res) {
   const feed = await parser.parseURL('https://www.lottery.co.th/lotto/feed');
   const contentSnippet = split("\r\n",feed.items[0].contentSnippet)
   const contentSnippetTranform = contentSnippet.map(transform)
-  res.status(400).json({ success: true, title: contentSnippetTranform[0][0] , jackpot: contentSnippetTranform[11][0] , two_digit: contentSnippetTranform[12][0] , tree_digit_prev: contentSnippetTranform[13][0], tree_digit_end: contentSnippetTranform[14][0]});
+  res.status(400).json({ success: true, title: contentSnippetTranform[0][0] , jackpot: contentSnippetTranform[11][0] , two_digit: contentSnippetTranform[12][0] , tree_digit_prev: contentSnippetTranform[13][0], tree_digit_end: contentSnippetTranform[14][0], pubDate:feed.items[0].pubDate});
 
 }
 export default ThLotory 
