b2a1450554c97c259aade40cd4220313dbe50d0c
userkmitl <59050300@kmitl.ac.th>
2018-08-17 13:33:21 +0700

    :imp: fix word

--- controllers/thlottory.js
+++ controllers/thlottory.js
@@ -1,28 +1,28 @@
 import Parser from 'rss-parser'
 import split from 'string-split'
-import striptags from 'striptags'
 const parser = new Parser();
 const ThLotory = async function (req, res) {
 
   const transform = split("\t");
+  const arr = ["มกราคม", "กุมภาพันธ์", "มีนาคม", "เมษายน", "พฤษภาคม", "มิถุนายน", "กรกฎาคม", "สิงหาคม", "กันยายน", "ตุลาคม", "พฤศจิกายน", "ธันวาคม"];
   const feed = await parser.parseURL('https://www.lottery.co.th/lotto/feed');
-  const contentSnippet = split("\r\n", feed.items[0].contentSnippet)
+  const index = 0;
+  const contentSnippet = split("\r\n", feed.items[index].contentSnippet)
   const contentSnippetTranform = contentSnippet.map(transform)
   const TmpjactpotDate = split(" ", contentSnippetTranform[0][0])
-  const arr = ["มกราคม", "กุมภาพันธ์", "มีนาคม", "เมษายน", "พฤษภาคม", "มิถุนายน", "กรกฎาคม", "สิงหาคม", "กันยายน", "ตุลาคม", "พฤศจิกายน", "ธันวาคม"];
   const jackpotDate = TmpjactpotDate[1] + "-" + arr.indexOf(TmpjactpotDate[2]) + "-" + TmpjactpotDate[3]
   const jackpotDateTh = TmpjactpotDate[1] + "-" + TmpjactpotDate[2] + "-" + TmpjactpotDate[3]
-  const tree_digit_prev = split(" ", contentSnippetTranform[13][0])
-  const tree_digit_end = split(" ", contentSnippetTranform[14][0])
+  const three_digit_prev = split(" ", contentSnippetTranform[13][0])
+  const three_digit_end = split(" ", contentSnippetTranform[14][0])
 
   res.status(400).json({
     success: true,
     title: contentSnippetTranform[0][0],
     jackpot: contentSnippetTranform[11][0],
     two_digit: contentSnippetTranform[12][0],
-    tree_digit_prev: tree_digit_prev,
-    tree_digit_end: tree_digit_end,
-    pubDate: feed.items[0].pubDate,
+    three_digit_prev: three_digit_prev,
+    three_digit_end: three_digit_end,
+    pubDate: feed.items[index].pubDate,
     jackpotDate: jackpotDate,
     jackpotDateTh: jackpotDateTh
   });
